afce856441189cc6f94353a7241d9b8b0bd83264
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-01 21:51:23 -0500

    add routes to allow users to post new messages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,14 @@ get '/' do
   @messages = Message.all
   erb :chat
 end
-post 'message/new' do
-  #..
+
+post '/message/new' do
+  message = Message.new(content: params[:message], sender: User.find(session[:user_id]))
+
+  if message.save
+    redirect('/')
+  else
+    redirect('/wtf')
+  end
+
 end
